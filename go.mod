3e6eecb41eb25368ad9111a64a2fe05ca9968c41
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2022-01-22 20:23:26 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module get-tickets
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var companyName = "TicketsGet"
+	fmt.Println("hello world! \nWelcome To ", companyName, "")
+	fmt.Println("Book yo Tickets fast..very fasst")
+
+}
