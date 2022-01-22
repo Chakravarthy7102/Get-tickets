570a20cd5660bfb1bf3101466b23504cc4b24db1
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2022-01-22 22:44:39 +0530

    blahhh

--- main.go
+++ main.go
@@ -1,32 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	companyName := "TicketsGet"
 	const totalTickets = 50
 	var availableTickets int = 50
+	bookings := []string{}
+
 	fmt.Printf("hello world! \nWelcome To %s \n", companyName)
 	fmt.Printf("Total Tickets are %d Available Tickets %d \n", totalTickets, availableTickets)
 	fmt.Println("Book yo Tickets fast..very fasst")
 
-	var firstName string
-	var lastName string
-	var email string
-	var ticketsBooked int
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var ticketsBooked int
 
-	fmt.Println("Enter Your first name")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter Your last name")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your Email")
-	fmt.Scan(&email)
-	fmt.Println("Enter no of ticket")
-	fmt.Scan(&ticketsBooked)
+		fmt.Println("Enter Your first name")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter Your last name")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your Email")
+		fmt.Scan(&email)
+		fmt.Println("Enter no of ticket")
+		fmt.Scan(&ticketsBooked)
 
-	availableTickets = availableTickets - ticketsBooked
+		for availableTickets-ticketsBooked < 0 {
+			fmt.Printf("Please Enter the ticket in range of %d : ", availableTickets)
+			fmt.Scan(&ticketsBooked)
+		}
+		availableTickets = availableTickets - ticketsBooked
 
-	fmt.Printf("You %s %s have booked %d tickets you will get a confirmation at this %s mail \n", firstName, lastName, ticketsBooked, email)
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("%d remaining ...\n", availableTickets)
+		fmt.Printf("You %s %s have booked %d tickets you will get a confirmation at this %s mail \n", firstName, lastName, ticketsBooked, email)
+
+		fmt.Printf("%d remaining ...\n", availableTickets)
+
+		firstNames := []string{}
+
+		for _, booking := range bookings {
+
+			firstname := strings.Fields(booking)
+			firstNames = append(firstNames, firstname[0])
+
+		}
+		fmt.Println(firstNames)
+
+		if availableTickets <= 0 {
+			fmt.Println("SOLD OUT!!")
+			break
+		}
+
+		fmt.Printf("All users %v \n", bookings)
+	}
 }
